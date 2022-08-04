d73bfa9060549013001b6bd6032acb3620e28da1
surahj <odebodeopeyemi@gmail.com>
2022-08-04 05:19:51 +0100

    Implement console.py

--- models/base_model.py
+++ models/base_model.py
@@ -2,6 +2,7 @@
 """Defines the BaseModel class."""
 from uuid import uuid4
 from datetime import datetime
+import models
 
 
 class BaseModel:
@@ -25,10 +26,13 @@ class BaseModel:
                     self.__dict__[k] = datetime.strptime(v, tform)
                 else:
                     self.__dict__[k] = v
+        else:
+            models.storage.new(self)
 
     def save(self):
         """Update updated_at with the current datetime."""
         self.updated_at = datetime.today()
+        models.storage.save()
 
     def to_dict(self):
         """Return the dictionary of the BaseModel instance.
