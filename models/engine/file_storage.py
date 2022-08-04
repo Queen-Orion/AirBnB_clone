391e75ab13e3ae1a2a04e145988efe7fd97e4bd9
surahj <odebodeopeyemi@gmail.com>
2022-08-04 15:51:54 +0100

    Update console.py

--- models/engine/file_storage.py
+++ models/engine/file_storage.py
@@ -33,7 +33,7 @@ class FileStorage:
     def reload(self):
         """Deserialize the JSON file __file_path to __objects, if it exists."""
         try:
-            with open(FileStorage.__file_path) as f:
+            with open(FileStorage.__file_path, encoding='utf-8', errors='ignore') as f:
                 objdict = json.load(f)
                 for o in objdict.values():
                     cls_name = o["__class__"]
